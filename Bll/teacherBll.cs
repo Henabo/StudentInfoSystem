ab9af1cbfd265f18ffa9089540417d6751d777c8
然 万 <1322980681@qq.com>
2019-11-25 12:58:54 +0800

    Bll更新

---
+++ Bll/teacherBll.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+using Dal;
+
+namespace Bll
+{
+    public class TeacherBll
+    {
+        TeacherDal tchDal = new TeacherDal();
+
+        public List<Teacher> SelectAllTeacher()  //所有老师
+        {
+            return tchDal.SelectAll();
+        }
+        public bool AddTeacher(Teacher tch)
+        {
+            return tchDal.Add(tch) > 0;
+        }
+        public bool DeleteTeacher(Teacher tch)
+        {
+            if (tchDal.Delete(tch) == -2)
+                return false;
+            else
+                return tchDal.Delete(tch);
+        }
+        public bool ModifyTeacher(Teacher tch) //修改
+        {
+            if (tchDal.Update(tch) == -2)
+                return false;
+            else
+                return tchDal.Update(tch);
+        }
+    }
+}
---
+++ Bll/teacherCourseBll.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+using Dal;
+
+namespace Bll
+{
+    public class TeacherCourseBll
+    {
+        TeacherCourseDal tchCourseDal = new TeacherCourseDal();
+
+        //查看老师课表
+        public List<TeacherCourse> SelectTeacherCourse(TeacherCourse tchCourse)
+        {
+            TeacherCourse tchCourse = tchCourseDal.SelectById();
+            list<Course> searchedCour = new list<Course>();
+            searchedCour.Add(tchCourse);
+            return searchedCour;
+        }
+    }
+}
